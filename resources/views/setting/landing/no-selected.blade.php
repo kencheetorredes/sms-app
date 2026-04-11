e431f77d804f5a1c72446e1bd71a077201c0192e
kencheetorredes <kencheetorredes@gmail.com>
2026-04-11 16:56:45 +0800

    update codes

--- resources/views/setting/landing/no-selected.blade.php
+++ resources/views/setting/landing/no-selected.blade.php
@@ -1,7 +1,8 @@
 <div class="chat status-middle-bar d-flex align-items-center justify-content-center">
         <div class="status-right">
             <div class="select-message-box">
-           
+            <h4>Select Setting</h4>
+            <p>to start manage</p>
             </div>
          </div>
     </div>
